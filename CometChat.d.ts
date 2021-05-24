1dbf3143351cf2a3cf5f4ed4388d581bcf82e4ed
Mayur Bhandari <mayur.bhandari@inscripts.com>
2021-05-24 15:30:51 +0530

    v3.0.0-beta4

--- CometChat.d.ts
+++ CometChat.d.ts
@@ -1965,7 +1965,10 @@ export namespace CometChat {
         shouldHideBlockedUsers: boolean;
         role: string;
         roles: Array<String>;
+        tags: Array<String>;
         showFriendsOnly: boolean;
+        showTags: boolean;
+        UIDs: Array<String>;
         setLimit(limit: number): this;
         setStatus(status: string): this;
         setSearchKeyword(searchKeyword: string): this;
@@ -1975,6 +1978,7 @@ export namespace CometChat {
         friendsOnly(friendsOnly: boolean): this;
         setTags(tags: Array<String>): this;
         withTags(withTags: boolean): this;
+        setUIDs(uids: Array<String>): this;
         build(): UsersRequest;
     }
 
@@ -2074,6 +2078,7 @@ export namespace CometChat {
         type?: string;
         types?: Array<String>;
         hideThreadedMessages?: boolean;
+        HideDeletedMessages?: boolean;
         setLimit(limit: number): this;
         setGUID(guid: string): this;
         setUID(uid: string): this;
@@ -2090,6 +2095,7 @@ export namespace CometChat {
         setType(type: string): this;
         setTypes(types: Array<String>): this;
         hideReplies(hideReplies: boolean): this;
+        hideDeletedMessages(hideDeletedMessages: boolean): this;
         /**
          *Built the DefaultMessagesRequest
         *
